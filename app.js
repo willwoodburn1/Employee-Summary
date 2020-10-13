cd7eb7c0c7741eb651c1043d41f2fbef06a9e8d8
User <user@Users-MacBook-Pro.local>
2020-10-13 12:59:48 +1000

    began work on the code using inquirer questions for employee

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,6 +10,44 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+
+function employeeQuestions() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please type in the employee's name.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please provide the employee's ID Number.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "please provide the employees email address.",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "Please select the job title for this particular employee.",
+            name: "position",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager"
+            ]
+        }
+
+    ]).then(function (response) {
+        if (response.position === "Engineer") {
+            employeeQuestions()
+            console.log("hey there");
+        }
+    })
+
+}
 
 
 
